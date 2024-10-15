b435fe23e7b7aeba2d06b6e77ecc0b5f7f677012
Hyunmin Hwang <hyunmin43240@gmail.com>
2024-10-15 12:14:33 +0900

    initial commit

---
+++ src/test/java/itm/calculatorTests.java
@@ -0,0 +1,33 @@
+package itm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import itm.oss.Calculator;
+
+@DisplayName("Calculator tests of OSS class")
+public class calculatorTests {
+
+    private final Calculator calc = new Calculator();
+
+    @Test
+    @DisplayName("Test for add function")
+    void add(){
+        assertEquals(10, calc.add(5,5), "Expected value is 10 in add function");
+    }
+
+    @Test
+    @DisplayName("Test for isPrime function")
+    void isfivePrime(){
+        assertTrue(calc.isPrime(5), "5 is prime");
+    }
+    @Test
+    @DisplayName("Test for isPrime function")
+    void isfourprime(){
+        assertFalse(calc.isPrime(4), "4 is not prime");
+    }
+}
